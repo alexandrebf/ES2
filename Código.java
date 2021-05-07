868b83dc8e2b63fe5ac48f2a974608ce2cec7c59
alexandrebf <61597419+alexandrebf@users.noreply.github.com>
2021-05-07 09:10:18 -0300

    Versão Final

---
+++ Código.java
@@ -0,0 +1,60 @@
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Formatter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        
+        System.out.println("CALCULO DE AREA E PERIMETRO");
+        System.out.println("0 para circulo || 1 para retangulo");
+        
+        int opcao;
+        Scanner lerOpcao = new Scanner(System.in);
+        opcao = lerOpcao.nextInt();
+        
+        if (opcao == 0){
+            
+            double pi = 3.14159;
+            double raio;
+            double area;
+            double per;
+            System.out.println("CALCULO DE AREA E PERIMETRO");
+            System.out.println("Informe o valor do raio referente ao circulo:");
+
+        
+            Scanner ler = new Scanner(System.in);
+            raio = ler.nextDouble();
+            area = pi*(raio*raio);
+            System.out.println("Area = "+String.format("%.2f", area));
+            
+            per = 2 * pi * raio;
+            System.out.println("Perimetro = "+String.format("%.2f", per));
+        }
+        else if (opcao == 1){
+            double areaR;
+            double perR;
+            double base;
+            double altura;
+            System.out.println("CALCULO DE AREA E PERIMETRO");
+            System.out.println("Informe o valor da base referente ao retangulo:");
+            
+            Scanner ler3 = new Scanner(System.in);
+            base = ler3.nextDouble();
+            
+            System.out.println("Informe o valor da altura referente ao retangulo:");
+            Scanner ler4 = new Scanner(System.in);
+            altura = ler4.nextDouble();
+    
+            
+            areaR = base * altura;
+            System.out.println("Area = "+String.format("%.2f", areaR));
+            
+            perR = 2 * (base + altura);
+            System.out.println("Perimetro = "+String.format("%.2f", perR));
+        }
+        else{
+             System.out.println("Digite uma opcao valida:");
+        }
+    }
+}
